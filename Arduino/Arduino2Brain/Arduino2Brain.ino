b0d52e19556cfb2185f1c0555753c190a7a24e97
Miguel Navarrete <miguel.angel.navarrete.meza@gmail.com>
2017-12-05 14:25:01 -0600

    Modified Arduino File

--- Arduino/Arduino2Brain/Arduino2Brain.ino
+++ Arduino/Arduino2Brain/Arduino2Brain.ino
@@ -2,8 +2,6 @@
 #include <MFRC522.h>
 #include <Servo.h>
 
-//TEst
-
 #define RST_PIN  9    
 #define SS_PIN  10 
 
@@ -11,7 +9,6 @@ int RoomLight1 = 3;
 int RoomLight2 = 5;
 int RoomLight3 = 6;
 int RoomLight4 = 11;
-
 
 Servo DoorsMotor;
 MFRC522 Lector1(SS_PIN, RST_PIN);
@@ -48,42 +45,43 @@ void loop() {
   }
 }
 
-//void serialEvent(){
-//  String input = Serial.readString();
-//
-//  int room_1 = getValue(input, ' ', 0).toInt();
-//  int room_2 = getValue(input, ' ', 1).toInt();
-//  int room_3 = getValue(input, ' ', 2).toInt();
-//  int room_4 = getValue(input, ' ', 3).toInt();
+void serialEvent(){
+  String input = Serial.readString();
+
+  int room_1 = getValue(input, ' ', 0).toInt();
+  int room_2 = getValue(input, ' ', 1).toInt();
+  int room_3 = getValue(input, ' ', 2).toInt();
+  int room_4 = getValue(input, ' ', 3).toInt();
+  String door_status = getValue(input, ' ', 4);
+
+  DoorHandler(door_status);
   
-//  LightHandlerRoom1(room_1,room_2,room_3,room_4);
-//}
-//
-//void DoorHandler(String DoorState) {
-//  int state = DoorState == "DoorO" ? 90 : 0;
-//  DoorsMotor.write(state);
-//}
-//
-//void LightHandler(int room_1, int room_2, int room_3, int room_4) {
-//   Serial.println(room_1,room_2,room_3,room_4);
-//   analogWrite(RoomLight1,room_1);
-//   analogWrite(RoomLight2,room_2);
-//   analogWrite(RoomLight3,room_3);
-//   analogWrite(RoomLight4,room_4);
-//}
-//
-//
-//String getValue(String data, char separator, int index) {
-//    int found = 0;
-//    int strIndex[] = { 0, -1 };
-//    int maxIndex = data.length() - 1;
-//
-//    for (int i = 0; i <= maxIndex && found <= index; i++) {
-//        if (data.charAt(i) == separator || i == maxIndex) {
-//            found++;
-//            strIndex[0] = strIndex[1] + 1;
-//            strIndex[1] = (i == maxIndex) ? i+1 : i;
-//        }
-//    }
-//    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-//}
+  LightHandler(room_1,room_2,room_3,room_4);
+}
+
+void DoorHandler(String DoorState) {
+  int state = DoorState == "DoorO" ? 90 : 0;
+  DoorsMotor.write(state);
+}
+
+void LightHandler(int room_1, int room_2, int room_3, int room_4) {
+   analogWrite(RoomLight1,room_1);
+   analogWrite(RoomLight2,room_2);
+   analogWrite(RoomLight3,room_3);
+   analogWrite(RoomLight4,room_4);
+}
+
+String getValue(String data, char separator, int index) {
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
